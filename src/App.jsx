b1cab4800adbdeee376c2c4ab48170897b4af34c
shameer125 <alishameer251@gmail.com>
2026-05-15 12:55:54 -0700

    add toast notification

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Layout from './layout/Layout';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-      </Route>
-    </Routes>
+    <>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
